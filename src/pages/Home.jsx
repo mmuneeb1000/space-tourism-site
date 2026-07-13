8ef2333d12d5ecd9daee11ed5eb6c374bb1d6de0
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-07-14 03:22:39 +0500

    image import to the app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <h1 className="p-10 text-4xl">Home</h1>;
+  return (
+    <section>
+      <h3>So, you want to travel to </h3>
+      <h2>Space</h2>
+      <p>
+        Let’s face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we’ll give you a truly out of this world
+        experience!
+      </p>
+      <button>Explore</button>
+    </section>
+  );
 }
