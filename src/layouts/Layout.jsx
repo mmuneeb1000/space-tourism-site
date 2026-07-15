23bb47a3c9e8b826a871713c1a615621e9ee55eb
mmuneeb1000 <muneebhashmi8@gmail.com>
2026-07-15 17:56:37 +0500

    background gradient added

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -6,13 +6,13 @@ export default function Layout() {
     <>
       <div
         className="
-    absolute flex justify-center left-7 inset-0 z-10 pointer-events-none mx-auto
-    max-w-[1400px]
-    bg-[length:25%_100%]
-    sm:bg-[length:20%_100%]
-    md:bg-[length:16.666%_100%]
-    lg:bg-[length:12.5%_100%]
-  "
+        absolute flex justify-center left-7 inset-0 z-10 pointer-events-none mx-auto
+        max-w-[1400px]
+        bg-[length:25%_100%]
+       sm:bg-[length:20%_100%]
+       md:bg-[length:16.666%_100%]
+       lg:bg-[length:12.5%_100%]
+         "
         style={{
           backgroundImage:
             "repeating-linear-gradient(to right, rgba(255,255,255,0.08) 0 65px, transparent 1px 100%)",
